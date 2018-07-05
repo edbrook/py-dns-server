89d38528442f5415fee0dc4231a0d84ad4645aee
Ed Brook <edbrook@gmail.com>
2018-07-05 12:19:16 +0100

    saving progress...

--- dnsserver/dnsserver.py
+++ dnsserver/dnsserver.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from socketserver import BaseRequestHandler, ThreadingMixIn, UDPServer
 
 
@@ -12,25 +13,86 @@ def get_dns_server(bind_address=None):
         dnsserver = DNSServer(bind_address, DNSRequestHandler)
     return dnsserver
 
+def _int_to_bytes(value, min_length=1):                                             
+    values = []
+    while value > 0 or len(values) < min_length:
+        values.append(value & 0xFF) 
+        value >>= 8
+    return bytes(values[::-1])
 
-class DNSRequestHandler(BaseRequestHandler):
-    def handle(self):
-        data, socket = self.request
-        # cli_ip, cli_port = self.client_address
-        id_ = data[0:2]
-        # https://www.ietf.org/rfc/rfc1035.txt
-        # QR:1 | Opcode:4 | AA:1 | TC:1 | RD:1
-        # RA:1 | Z:3 | RCODE:4 
+def _bytes_to_int(bytes_):
+    i = 0
+    for byte in bytes_:
+        i <<= 8
+        i += int(byte) 
+    return i
+
+
+def display(data):
+    bits = [bin(data[n])[2:].zfill(8) for n in range(len(data))]
+    print('\n'.join([f'{bits[n]} {bits[n+1]}' for n in range(0, len(bits),2)]))
+
+
+class DNSqr(Enum):
+    QUERY = 0
+    RESPONSE = 1
+
+
+class DNSOpCode(Enum):
+    QUERY = 0
+    IQUERY = 1
+    STATUS = 2
+
+
+class DNSRCode(Enum):
+    NOERROR = 0
+    FORMERR = 1
+    SERVFAIL = 2
+    NXDOMAIN = 3
+    NOTIMP = 4
+    REFUSED = 5
+
+
+
+class DNSResponse:
+    def __init__(self, id_):
+        # See - https://www.ietf.org/rfc/rfc1035.txt
+        self.id_ = id_
+        self.qr = DNSqr.QUERY
+        self.opcode = DNSOpCode.QUERY
+        self.aa = True
+        self.tc = False
+        self.rd = True
+        self.ra = False
+        self.z = 0
+        self.rcode = DNSRCode.NXDOMAIN
+        self.querys = []
+        self.answers = []
+        self.authories = []
+        self.additionals = []
+    
+    def get_response(self):
         codes = bytes([0b10000001, 0b10000000])
         qd_count = b'\x00\x00'
         an_count = b'\x00\x00'
         ns_count = b'\x00\x00'
         ar_count = b'\x00\x00'
-        res =  id_ + codes + qd_count + an_count + ns_count + ar_count
+        return _int_to_bytes(self.id_) + codes + qd_count + an_count + ns_count + ar_count
+
+
+class DNSRequestHandler(BaseRequestHandler):
+    def handle(self):
+        data, socket = self.request
+        # cli_ip, cli_port = self.client_address
+        id_ = _bytes_to_int(data[0:2])
+        res = DNSResponse(id_).get_response()
+        
         print(f'<<< {data}')
+        display(data)
+        
         print(f'>>> {res}')
-        # bits = [bin(data[n])[2:].zfill(8) for n in range(len(data))]
-        # print('\n'.join([f'{bits[n]} {bits[n+1]}' for n in range(0, len(bits),2)]))
+        display(res)
+        
         socket.sendto(res, self.client_address)
 
 
