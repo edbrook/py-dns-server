704f2007c38684002169e0c3cccd55ea2e04aa22
Ed Brook <edbrook@gmail.com>
2018-07-07 12:14:55 +0100

    handle permission denied error: e.g. trying to use priv port as non-root

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 from dnsserver import dnsserver
 
 def main():
-    bind_address = ('127.0.0.1', 1053)
+    bind_address = ('127.0.0.1', 53)
+    server = None
     try:
         server = dnsserver.get_dns_server(bind_address)
         server.serve_forever()
+    except PermissionError:
+        print('Permission denied - need to run as root?')
     except KeyboardInterrupt:
         print('Exiting...')
     finally:
